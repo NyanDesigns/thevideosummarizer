981876ccfbd77ceb83599e9cb18fec7dc2b3e6b8
NyanDesigns <nmyat@cpp.edu>
2024-05-28 12:24:32 -0700

    first-commit-template

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// .eslintrc.js
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
