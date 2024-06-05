03a458677a2bf0ba1097ae6d3624d2741b671840
NyanDesigns <nmyat@cpp.edu>
2024-06-04 19:55:16 -0700

    renameTitleBlock

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,9 +1,8 @@
 "use client";
 
-//icons
-import { FaPowerOff } from "react-icons/fa";
 //customComponents
 import NoSSRWrapper from "../../lib/NoSSRWrapper";
+import { TitleBlock } from "./TitleBlock";
 import { VideoBlock } from "./VideoBlock";
 
 export function Hero() {
@@ -14,30 +13,8 @@ export function Hero() {
       <div className="flex flex-col h-full max-w-4xl px-6 mt-24 mb-10 sm:mt-28">
         {/* hearoWarper */}
         <div className="space-y-4 sm:space-y-6">
-          {/* heading */}
-          <h1 className="text-4xl font-bold text-center text-white sm:text-5xl sm:leading-tight">
-            Summarize Any Video,
-            <br className="mt-2 max-sm:hidden" />
-            <span className="text-transparent bg-gradient-to-r from-red-500 via-yellow-500 to-red-500 bg-clip-text">
-              Visually & Audibly,
-            </span>
-            <br className="mt-2 max-sm:hidden" />
-            w/ Artificial Intelligence
-          </h1>
-          {/* bubbleTag */}
-          <div className="flex flex-col items-center w-full">
-            {/* bubble */}
-            <div className="flex w-fit flex-row items-center gap-2 rounded-[20px] border border-yellow-500 px-2 py-1 text-[10px] text-yellow-500 ">
-              <FaPowerOff />
-              Powered by GPT-4o
-            </div>
-          </div>
-          {/* subHeading */}
-          <p className="pb-4 text-center text-slate-400 sm:pb-8">
-            Cut through the noise and get to the point. Let our AI-powered tool
-            summarize videos for you, delivering the key highlights in a
-            fraction of the time.
-          </p>
+          {/* titleBlock */}
+          <TitleBlock />
           {/* FormBlock */}
           <VideoBlock />
         </div>
